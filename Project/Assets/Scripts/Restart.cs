796c06de8fe33a863ebff3f51942fb8598ac16dc
jjj0323 <89474308+jjj0323@users.noreply.github.com>
2023-03-08 13:52:33 -0600

    update

---
+++ Project/Assets/Scripts/Restart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
+
+
